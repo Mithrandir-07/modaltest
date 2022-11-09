46e12a6727cab0cf2a12900129bc7c4b25c70259
Mithrandir-07 <KostyaBaliuk7@gmail.com>
2022-11-09 12:21:30 +0200

    firstcommit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
+import { useForm } from "react-hook-form";
+
 
 export default function MyModal() {
   let [isOpen, setIsOpen] = useState(true)
@@ -24,39 +26,11 @@ export default function MyModal() {
 
       <main className={styles.main}>
 
-        <header class="container mx-auto px-5">
-
-          <div class="flex justify-between pt-10">
-
-            <span class="fond-bpld text-3xl">
-              Tailwind
-            </span>
-
-            <div class="space-x-5">
-              <a href="" class="hover:underline">About</a>
-              <a href="" class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Contacts</a>
-            </div>
-          </div>
-        </header> 
-
-        <div class="container mx-auto py-big px-3 sm:px-0">
-          <h1 class="text-center text-3xl fond-bold">TailwindCSS</h1>
-          <p class="pt-3 w-full sm:w-[450px] mx-auto ">The quickest way to start using Tailwind CSS in your Next.js project is to use the Next.js + Tailwind CSS Example. This will automatically configure your Tailwind setup based on the official Next.js example. If you'd like to configure Tailwind manually, continue with the rest of this guide.</p>
-        </div>   
-
-        <div class="flex justify-center space-x-0 md:space-x-10 flex-col md:flex-row space-y-5 md:space-y-0 px-3 sm:px-0">
-          <div class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Button1</div>
-          <div class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Button2</div>
-          <div class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Button3</div>
-          <div class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Button4</div>
-          <div class="bg-red-500 py-2 px-4 rounded-full hover:bg-red-700">Button5</div>
-        </div>
-
         <div className="fixed inset-0 flex items-center justify-center">
           <button
             type="button"
             onClick={openModal}
-            className="rounded-full bg-black px-4 py-2 text-sm font-medium text-white hover:bg-opacity-60 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+            className="rounded-full bg-black px-4 py-2 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
           >
             Open dialog
           </button>
